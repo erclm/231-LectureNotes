2138472c5bb9086b82f8465361bee3cb532c71a5
studebaker <studebaker@fse-studebaker-mbp.chapman.edu>
2023-02-16 12:45:41 -0800

    uploaded the checkerboard with borders code

--- 04_arrays/practice/Checkerboard.java
+++ 04_arrays/practice/Checkerboard.java
@@ -1,10 +1,10 @@
 public class Checkerboard {
     public static void main(String[] args) {
-        char[][] board = new char[5][5];
+        char[][] board = new char[10][5];
         
         for (int row=0; row<board.length; ++row) {
             for (int col=0; col<board[row].length; ++col) {
-                if (row==0 || col==0) {
+                if (row == 0 || col == 0 || row == board.length-1 || col == board[row].length-1) {
                     board[row][col] = 'O';
                 } else {
                     board[row][col] = 'X';
